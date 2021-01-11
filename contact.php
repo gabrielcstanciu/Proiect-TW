695ba6b3c7e5013b85f73a502eedb7c6cbf3c241
gabrielcstanciu <52375769+gabrielcstanciu@users.noreply.github.com>
2021-01-11 17:57:41 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,113 @@
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='contact.php'</script>";
+
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>HMS | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+		
+					<img src="images/logo.png" style="center">
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html" style="color:red">Home</a></li>
+					
+						<li class="active"><a href="contact.php" style="color:red">contact</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>Medical X Address  :</h2>
+						    	<p> B-dul. Republicii nr. 21 (zona "Bega Electromotor")</p>
+						   		<p>Timisoara</p>
+						   		<p>Romania</p>
+				   		<p>Phone:0256 326 789</p>
+				 	 	<p>Email: <span>contact@medicalx.ro</span></p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form name="contactus" method="post">
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" name="fullname" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="email" name="emailid" required="ture" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" name="mobileno" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Description</label></span>
+						    	<span><textarea name="description" required="true"> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" name="submit" value="Submit" style="color:red"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html" style="color:red">Home</a></li>
+						
+						<li><a href="contact.php" style="color:red">contact</a></li>
+					</ul>
+		   	</div>
+		  
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
