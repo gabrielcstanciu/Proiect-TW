ff3227179119af1d04da69a8bc8561c528c66e2f
gabrielcstanciu <52375769+gabrielcstanciu@users.noreply.github.com>
2021-01-11 20:35:57 +0200

    Add files via upload

--- checklogin.php
+++ checklogin.php
@@ -5,7 +5,7 @@ if(strlen($_SESSION['login'])==0)
 	{	
 		$host = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra="./index.php";		
+		$extra="../admin.php";		
 		$_SESSION["login"]="";
 		header("Location: http://$host$uri/$extra");
 	}
